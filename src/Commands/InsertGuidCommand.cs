e5a4728e79f0f761f3e5bd922b1efc7add11643f
Mads Kristensen <post@madskristensen.net>
2018-03-22 14:54:04 -0700

    Add project files.

---
+++ src/Commands/InsertGuidCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Windows.Forms;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace InsertGuid
+{
+    internal sealed class InsertGuidCommand
+    {
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            if (await package.GetServiceAsync(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var cmdId = new CommandID(PackageGuids.guidInsertGuidCommandPackageCmdSet, PackageIds.InsertGuidCommandId);
+                var menuItem = new MenuCommand(Execute, cmdId);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        private static void Execute(object sender, EventArgs e)
+        {
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+            Command cmd = dte.Commands.Item("Edit.Paste");
+
+            if (cmd != null && cmd.IsAvailable)
+            {
+                string existing = Clipboard.GetText();
+                Clipboard.SetText(Guid.NewGuid().ToString(), TextDataFormat.Text);
+                dte.Commands.Raise(cmd.Guid, cmd.ID, null, null);
+                Clipboard.SetText(existing);
+            }
+            else
+            {
+                Clipboard.SetText(Guid.NewGuid().ToString(), TextDataFormat.Text);
+                dte.StatusBar.Text = "New GUID copied to clipboard";
+            }
+        }
+    }
+}
---
+++ src/InsertGuidPackage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace InsertGuid
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]       
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidInsertGuidCommandPackageString)]
+    public sealed class InsertGuidPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await InsertGuidCommand.InitializeAsync(this);
+        }
+    }
+}
